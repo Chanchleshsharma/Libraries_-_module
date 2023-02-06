c29310f6b3dd9b78f1eb6561a58f141c362b241f
chanchleshsharma <chanchleshsharma123@gmail.com>
2023-02-06 23:56:33 +0530

    new commit

---
+++ 13Constructor.py
@@ -0,0 +1,10 @@
+# Constructor :--> It is a special method of class 
+#__init__():
+
+class account:
+    def __init__(self):
+        print("this is Constructor")
+
+
+a=account()
+b=account()
---
+++ 14_constructor.py
@@ -0,0 +1,10 @@
+class account:
+    ifsc="abc984"
+
+    def __init__(self):
+        self.sal=10000
+
+a=account()
+b=account()
+print(a.sal,b.sal)
+print(a.ifsc,b.ifsc)
